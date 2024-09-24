lairdc/SantoriniAI

commit 59ceb2e535b0e5f72ac619404f40ad9053398504

chore: add type hints to parameters (#4)
* refactor: cleanup imports

* chore: add type hinting to params

santoriniGame/game.py
@@ -1,12 +1,15 @@
 import pygame
-from .constants import *
+
 from .board import Board
+from .constants import *
+
 
 class Game:
-    def __init__(self, win):
+    def __init__(self, win: pygame.SurfaceType):
         self._init()
         self.win = win
         self.game_over = None  # Start with None, to be set as 'BLUE' or 'RED' on win
+        self.turn = None
 
     def update(self):
         self.board.draw(self.win, self.valid_moves)
@@ -23,7 +26,7 @@ class Game:
     def reset(self):
         self._init()
 
-    def select(self, row, col):
+    def select(self, row: int, col: int):
         piece = self.board.get_piece(row, col)
 
         if self.selected:
@@ -56,7 +59,7 @@ class Game:
         self.change_turn()  # Change turn after building
         self.selected = None  # Deselect after building
 
-    def _move(self, row, col):
+    def _move(self, row: int, col: int):
         if self.selected and (row, col) in self.valid_moves:
             self.board.move(self.selected, row, col)
 
@@ -72,7 +75,7 @@ class Game:
             return True
         return False
 
-    def display_winner(self, winner_color):
+    def display_winner(self, winner_color: tuple[int, int, int]):
         font = pygame.font.SysFont(None, 72)
         win_text = f"{'Blue' if winner_color == BLUE else 'Red'} Wins!"
         text = font.render(win_text, True, (255, 255, 255))  # White text

santoriniGame/board.py
@@ -1,4 +1,5 @@
 import pygame
+
 from .constants import *
 from .pieces import Piece
 
@@ -11,7 +12,7 @@ class Board:
         self.create_board()
         self.font = pygame.font.SysFont(None, 36)  # Font for displaying tile levels
 
-    def draw(self, win, valid_moves=None):
+    def draw(self, win: pygame.SurfaceType, valid_moves=None):
         win.fill(GREEN)
 
         # Draw the grid
@@ -30,7 +31,7 @@ class Board:
 
                 # Highlight valid moves
                 if valid_moves and (row, col) in valid_moves:
-                    pygame.draw.circle(win, HIGHLIGHT_COLOR, 
+                    pygame.draw.circle(win, HIGHLIGHT_COLOR,
                                        (x + SQUARE_SIZE // 2, y + SQUARE_SIZE // 2), 10)
 
         # Draw the pieces
@@ -38,7 +39,7 @@ class Board:
             piece.draw(win)
 
 
-    def move(self, piece, row, col):
+    def move(self, piece: Piece, row: int, col: int):
         # Update piece location and level
         if piece in self.board:
             self.board.remove(piece)
@@ -46,7 +47,7 @@ class Board:
             piece.move(row, col, new_level)
             self.board.append(piece)
 
-    def get_piece(self, row, col):
+    def get_piece(self, row: int, col: int) -> Piece | None :
         # Check if a piece exists at the given row and col
         for piece in self.board:
             if piece.row == row and piece.col == col:
@@ -62,7 +63,7 @@ class Board:
         self.board.append(Piece(ROWS - 2, 1, RED))  # Bottom-left corner
         self.board.append(Piece(ROWS - 2, COLS - 2, RED))  # Bottom-right corner
 
-    def get_valid_moves(self, piece):
+    def get_valid_moves(self, piece: Piece):
         moves = {}
         directions = [
             (-1, -1), (-1, 0), (-1, 1),  # Top-left, top, top-right
@@ -85,7 +86,7 @@ class Board:
 
         return moves
 
-    def get_valid_builds(self,piece):
+    def get_valid_builds(self, piece: Piece):
 
         builds = {}
         directions = [

santoriniGame/constants.py
@@ -1,5 +1,3 @@
-import pygame
-
 WIDTH, HEIGHT = 800,800
 
 ROWS, COLS = 5,5

santoriniGame/main.py
@@ -1,6 +1,6 @@
 import pygame
+
 from santoriniGame.constants import WIDTH, HEIGHT, SQUARE_SIZE
-from santoriniGame.board import Board
 from santoriniGame.game import Game
 
 FPS = 60
@@ -8,7 +8,7 @@ FPS = 60
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Santorini')
 
-def get_row_col_from_mouse(pos):
+def get_row_col_from_mouse(pos: tuple[int, int]):
     x, y = pos
     row = y // SQUARE_SIZE
     col = x // SQUARE_SIZE

santoriniGame/pieces.py
@@ -1,11 +1,12 @@
 import pygame
+
 from .constants import *
 
 class Piece:
 	PADDING = 15
 	BORDER = 2
 
-	def __init__(self,row,col,color):
+	def __init__(self, row: int, col: int, color: tuple[int, int, int]):
 		self.row = row
 		self.col = col
 		self.color = color
@@ -19,11 +20,11 @@ class Piece:
 		self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
 		self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
 
-	def draw(self, win):
+	def draw(self, win: pygame.SurfaceType):
 		radius = SQUARE_SIZE//2 - self.PADDING
 		pygame.draw.circle(win, self.color, (self.x, self.y), radius)
 
-	def move(self,row,col,level):
+	def move(self, row: int, col: int, level: int):
 		self.row = row
 		self.col = col
 		self.level = level

commit c378efc18a5e75fb7398a29aa79b4e46b54d0547

fix: regression where no move can be made
Fixes a regression introduced in 59ceb2e535b0e5f72ac619404f40ad9053398504

santoriniGame/game.py
@@ -9,7 +9,6 @@ class Game:
         self._init()
         self.win = win
         self.game_over = None  # Start with None, to be set as 'BLUE' or 'RED' on win
-        self.turn = None
 
     def update(self):
         self.board.draw(self.win, self.valid_moves)