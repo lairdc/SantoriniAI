ad7f61650f52025de66bfd074fae43270da1648e
Richard Sun <sunr5@rpi.edu>
2024-09-24 16:54:05 -0400

    chore: add type hinting to params

--- santoriniGame/game.py
+++ santoriniGame/game.py
@@ -5,10 +5,11 @@ from .constants import *
 
 
 class Game:
-    def __init__(self, win):
+    def __init__(self, win: pygame.SurfaceType):
         self._init()
         self.win = win
         self.game_over = None  # Start with None, to be set as 'BLUE' or 'RED' on win
+        self.turn = None
 
     def update(self):
         self.board.draw(self.win, self.valid_moves)
@@ -25,7 +26,7 @@ class Game:
     def reset(self):
         self._init()
 
-    def select(self, row, col):
+    def select(self, row: int, col: int):
         piece = self.board.get_piece(row, col)
 
         if self.selected:
@@ -58,7 +59,7 @@ class Game:
         self.change_turn()  # Change turn after building
         self.selected = None  # Deselect after building
 
-    def _move(self, row, col):
+    def _move(self, row: int, col: int):
         if self.selected and (row, col) in self.valid_moves:
             self.board.move(self.selected, row, col)
 
@@ -74,7 +75,7 @@ class Game:
             return True
         return False
 
-    def display_winner(self, winner_color):
+    def display_winner(self, winner_color: tuple[int, int, int]):
         font = pygame.font.SysFont(None, 72)
         win_text = f"{'Blue' if winner_color == BLUE else 'Red'} Wins!"
         text = font.render(win_text, True, (255, 255, 255))  # White text
