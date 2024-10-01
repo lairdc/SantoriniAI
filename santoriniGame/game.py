2bb32c93a59ee9d47edf3ba6e194f5b1d8d5f819
Richard Sun <sunr5@rpi.edu>
2024-10-01 17:24:04 -0400

    chore(pygame): add type hints to parameters (#15)

--- santoriniGame/game.py
+++ santoriniGame/game.py
@@ -1,5 +1,6 @@
 import pygame
 
+from pieces import Piece
 from .board import Board
 from .constants import *
 
@@ -15,7 +16,7 @@ class Game:
         pygame.display.update()
 
     def _init(self):
-        self.selected = None
+        self.selected: Piece | None = None
         self.board = Board()
         self.turn = BLUE
         self.valid_moves = {}
@@ -50,7 +51,7 @@ class Game:
 
         return False
 
-    def _build(self, row, col):
+    def _build(self, row: int, col: int):
         self.board.tile_levels[row][col] += 1
         print(f"Built at ({row}, {col}), New level: {self.board.tile_levels[row][col]}")  # Debugging output
         self.valid_moves = {}
