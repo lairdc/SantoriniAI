4aa0b0c4ef5259d700634288dd9c3e891f6548e3
Richard Sun <sunr5@rpi.edu>
2024-10-08 17:24:07 -0400

    feat(web/client): add react-router-dom to allow for multiple pages

--- santoriniWebsite/client/src/App.tsx
+++ santoriniWebsite/client/src/App.tsx
@@ -1,41 +1,13 @@
-import './fonts.css';
-import './App.css'; 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import IndexPage from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          <span className="welcome">Welcome to </span>
-          <span className="santorini">SantoriniAI</span>
-          <span className="welcome"> Board Game!</span>
-        </h1>
-      </header>
-
-      <div className="game-overview">
-        <div className="game-overview-left">
-          <div>
-          <h2><b>Game Overview</b></h2>
-          <p>Santorini is a strategic board game where players to move their workers and construct buildings across the evolving board. 
-            The goal is to maneuver your workers to the top of a three-tiered structure. Players can also harness the power of Greek gods, 
-            adding unique abilities that influence the flow of the game.
-          </p>
-          </div>
-          
-          <div className="button-container">
-            <button onClick={() => alert('Playing against a friend')}>
-              Play Against Friend
-            </button>
-            <button onClick={() => alert('Playing against AI')}>
-              Play Against AI
-            </button>
-          </div>
-        </div>
-        <div className="game-overview-img">
-          <img src="/board.png" alt="Santorini Game" />
-        </div>
-      </div>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<IndexPage />}></Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
