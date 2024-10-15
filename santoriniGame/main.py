bf2a04015f188bd4d91aaf86dc718a450482c1ca
yasemin-et <yasemin.elif.turkoglu@outlook.com>
2024-10-15 14:24:44 -0400

    Some files were broken from main, made small changes to import lines to fix it

--- santoriniGame/main.py
+++ santoriniGame/main.py
@@ -4,6 +4,7 @@ from santoriniGame.constants import WIDTH, HEIGHT, SQUARE_SIZE, BLUE, RED, GREY,
 from santoriniGame.game import Game
 from santoriniGame.bot import Bot
 from santoriniGame.ColbysMiniMax import ColbysMiniMax
+from santoriniGame.YaseminsMiniMax import YaseminsMiniMax
 
 FPS = 60
 
@@ -100,14 +101,14 @@ def main():
         blue_player = None
         red_player = None
     elif game_mode == "CvP":
-        blue_player = ColbysMiniMax(game, BLUE, RED)
+        blue_player = YaseminsMiniMax(game, BLUE, RED)
         red_player = None
     elif game_mode == "PvC":
         blue_player = None
-        red_player = ColbysMiniMax(game,RED,BLUE)
+        red_player = YaseminsMiniMax(game,RED,BLUE)
     else:
         blue_player = Bot(game,BLUE,RED)
-        red_player = ColbysMiniMax(game,RED,BLUE)
+        red_player = YaseminsMiniMax(game,RED,BLUE)
 
     while run:
         clock.tick(FPS)
