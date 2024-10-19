2292365ea17c2b18731b4b993c06221eade85881
LordBowlingBall <tycswong@gmail.com>
2024-10-19 16:37:13 -0400

    Added more comments and improved code

--- santoriniGame/TylerMiniMax.py
+++ santoriniGame/TylerMiniMax.py
@@ -13,13 +13,13 @@ class TylerMiniMax:
         move_level = board.get_tile_level(move_x,move_y)
         diff = move_level - cur_level
         score = 0
-        if move_level == 3:  # Winning ove
+        if move_level == 3:  # Winning move
            score = 100000
         elif diff == 0:  # Staying on the same level
             score = 0
         else:  #Moving level
             score = 500 * diff
-        return score + self.dist_from_opp(opp_pieces,move_x,move_y)
+        return score + self.dist_from_opp(opp_pieces,move_x,move_y)  # Tries to move far from opponent
 
     #Checks if the oppoent can move to the given pos
     def opp_can_move(self, board, opp_pieces, action_x, action_y):
@@ -41,7 +41,7 @@ class TylerMiniMax:
             elif tile_level == 2:  # Doesn't give winning move to opponent
                 score = -50000
         else:
-            # Tries building up
+            # Tries building up and away from opponent
             if tile_level > 3:
                 score = -100 * board.get_tile_level(build_x, build_y) + self.dist_from_opp(opp_pieces,build_x,build_y)
             else:
@@ -72,7 +72,7 @@ class TylerMiniMax:
                 # Simulate the piece moving to the location for build
                 fake_piece = Piece(move[0],move[1],self.own_color)
                 valid_builds = self.game.board.get_valid_builds(fake_piece)
-                # Calulates score for the pieces current pos before they moved
+                # Calculates score for the pieces current pos before they moved
                 valid_builds[(own_pieces[i].get_x(), own_pieces[i].get_y())] = board.get_tile_level(own_pieces[i].get_x(), own_pieces[i].get_y())
                 for build in valid_builds:
                     opp_move = self.opp_can_move(board, opp_pieces, build[0], build[1])
@@ -83,7 +83,7 @@ class TylerMiniMax:
                     actions_size += 1
                     if score > actions[highest_score_index][5]:
                         highest_score_index = actions_size
-                    elif score == actions[highest_score_index][5]:  # If scores are equal, it randomly chooses to change the action
+                    elif score == actions[highest_score_index][5]:  # Randomly changes same score actions
                         rand = random.randint(0,1)
                         if rand == 0:
                             highest_score_index = actions_size
@@ -95,20 +95,18 @@ class TylerMiniMax:
 
     def make_move(self):
         own_pieces = self.game.board.get_all_pieces(self.own_color)
-        opp_pieces = self.game.board.get_all_pieces(self.opp_color)
 
         if not own_pieces:
             return  # No pieces to move
 
         action = self.get_best_action(self.game.board)
         piece = own_pieces[action[0]]
-        valid_moves = self.game.board.get_valid_moves(piece)
 
         if action is not None:
             move = [action[1],action[2]]
             row, col = move
             if self.game.select(piece.row, piece.col):  # Simulate piece selection
-                self.game._move(row, col)  # Move to a random valid position
+                self.game._move(row, col)  # Moves bot piece
                 build = [action[3],action[4]]
                 build_row, build_col = build
                 self.game._build(build_row, build_col)
