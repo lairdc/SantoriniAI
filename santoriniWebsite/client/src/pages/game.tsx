e42ddcd397cbf3f9b981c20399753f06e9ed14f2
Richard Sun <sunr5@rpi.edu>
2024-11-01 17:08:15 -0400

    feat(web/client): Improve frontend visualization (#42)
    * feat(web/client): keep track of win state
    
    * feat(web/client): mark valid moves with an indicator
    
    CC-BY-SA 4.0 https://stackoverflow.com/a/5670984

--- santoriniWebsite/client/src/pages/game.tsx
+++ santoriniWebsite/client/src/pages/game.tsx
@@ -22,11 +22,13 @@ export default function GamePage() {
         }
 
         return (
-            <button className={`game-space game-space-built-${game.board.getTileLevel(row, col)} ${update}`} onClick={()=>{
-                if (!game.select(row, col)) {
-                    game.selected = null;
+            <button className={`game-space game-space-built-${game.board.getTileLevel(row, col)} ${`${row}-${col}` in game.validMoves ? "game-space-valid" : ""} ${update}`} onClick={()=>{
+                if (!game.gameOver) {
+                     if (!game.select(row, col)) {
+                        game.selected = null;
+                    }
+                    forceUpdate(update+1);
                 }
-                forceUpdate(update+1);
             }}><Piece row={row} col={col}></Piece></button>
         );
     }
@@ -41,7 +43,7 @@ export default function GamePage() {
     const gridded = spaces.map((row, i) => <div className={"game-row"} key={`row-${i}`}>{row}</div>);
 
     return [
-        <span key="game-state">{game.turn}'s turn to {game.move ? "move" : "build"}</span>,
+        <span key="game-state">{game.gameOver ? `${game.gameOver} wins!` : `${game.turn}'s turn to ${game.move ? "move" : "build"}`}</span>,
         ...gridded
     ]
 }
