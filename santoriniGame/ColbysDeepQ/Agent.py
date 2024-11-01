2fd45e7f42de6d591e3bcc1b7cd6530377278d4e
ColbyLairdieson <cjlclimber@gmail.com>
2024-11-01 17:19:40 -0400

    adding pytorch imports

--- santoriniGame/ColbysDeepQ/Agent.py
+++ santoriniGame/ColbysDeepQ/Agent.py
@@ -1,4 +1,8 @@
-import torch
+import torch as T 
+import torch.nn as nn
+import torch.nn.functional as F 
+import torch.optim as optim
+import numpy as np 
 
 
 
---
+++ santoriniGame/ColbysDeepQ/DeepQ.py
@@ -0,0 +1,6 @@
+import torch as T 
+import torch.nn as nn
+import torch.nn.functional as F 
+import torch.optim as optim
+import numpy as np 
+
