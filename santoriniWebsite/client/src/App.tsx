51586d93860067938d368a8a5eb021a193cfa2f3
Richard Sun <sunr5@rpi.edu>
2024-11-01 16:48:49 -0400

    feat(web/client): initial frontend board (#36)
    * feat(web/client): initial frontend board
    
    * fix(web/client): grid blocks keep shifting
    
    this causes the piece text to not be centered though

--- santoriniWebsite/client/src/App.tsx
+++ santoriniWebsite/client/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes, Link} from "react-router-dom";
+import GamePage from "./pages/game";
 import IndexPage from "./pages/index";
 import RulesPage from "./pages/rules";
 import './App.css';
@@ -9,10 +10,12 @@ function App() {
         <nav className="nav-bar">
           <Link to="/" className="nav-button">Home</Link>
           <Link to="/rules" className="nav-button">Rules</Link>
+          <Link to="/game" className="nav-button">Play</Link>
         </nav>
         <Routes>
           <Route path="/" element={<IndexPage />}></Route>
           <Route path="/rules" element={<RulesPage />} /> 
+          <Route path="/game" element={<GamePage />} />
         </Routes>
       </BrowserRouter>
   );
