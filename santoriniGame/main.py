6dcc3a4676b4aba1f8eaec45ca5b8dea68563737
Richard Sun <sunr5@rpi.edu>
2024-11-15 16:39:15 -0500

    Implement the bots API (#47)
    * fix: imports don't have module name
    
    * fix(web/server): add module to santoriniWebsite
    
    * refactor: rename bot.py to randombot.py
    
    * feat: add no_render argument to game.py
    
    * feat(web/server): implement all api endpoints
    
    * fix(web/server): serialization bug on bots endpoint

--- santoriniGame/main.py
+++ santoriniGame/main.py
@@ -5,9 +5,9 @@ import time  # Import time module
 
 
 
-from constants import WIDTH, HEIGHT, SQUARE_SIZE, BLUE, RED, GREY, GREEN
-from game import Game
-from bot import Bot
+from santoriniGame.constants import WIDTH, HEIGHT, SQUARE_SIZE, BLUE, RED, GREY, GREEN
+from santoriniGame.game import Game
+from santoriniGame.randombot import RandomBot
 from TylerMiniMax.TylerMiniMax import TylerMiniMax
 from YaseminsMiniMax.YaseminsMiniMax import YaseminsMiniMax
 from ColbysMiniMax.ColbysMiniMax import *
@@ -115,7 +115,7 @@ def main():
         blue_player = None
         red_player = YaseminsMiniMax(game,RED,BLUE)
     else:
-        blue_player = Bot(game,BLUE,RED)
+        blue_player = RandomBot(game, BLUE, RED)
         red_player = YaseminsMiniMax(game,RED,BLUE)
 
 
